4107b4003ac931f0f49734c919f75e7a4de7e3a6
bb1950328 <36961718+bb1950328@users.noreply.github.com>
2020-01-15 14:35:18 +0100

    Update compare_output.sh

--- .github/workflows/compare_output.sh
+++ .github/workflows/compare_output.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #LINE=$(tail -1 ./.github/workflows/unittest_output.txt)
-cat ./.github/workflows/unittest_output.txt
-LINE=$(tac ./.github/workflows/unittest_output.txt |egrep -m 1 .)
+cat ./unittest_output.txt
+LINE=$(tac ./unittest_output.txt |egrep -m 1 .)
 echo "The last line is: $LINE"
 [[ $LINE == *"OK"* ]] && exit 0 || exit -1
