661ec1d07487a79747c5f4fc410c82a63c09c9c6
bb1950328 <bb1950328@gmail.com>
2020-01-29 19:21:16 +0100

    fixed syntax

--- .github/workflows/commit_if_changed.sh
+++ .github/workflows/commit_if_changed.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 #TODO chmod a+x this file
-file_count = "$(git diff --cached --numstat | wc -l)"
-if ["$file_count" != "0"]
+file_count="$(git diff --cached --numstat | wc -l)"
+if "$file_count" != "0"
 then
   git commit -am "Rescaled images (Github Action)"
   git push
