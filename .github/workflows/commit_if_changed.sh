cb20ca2859aa20027a579993b15dc798969c5cb6
bb1950328 <36961718+bb1950328@users.noreply.github.com>
2020-01-30 09:59:52 +0100

    Update commit_if_changed.sh

--- .github/workflows/commit_if_changed.sh
+++ .github/workflows/commit_if_changed.sh
@@ -2,7 +2,7 @@
 
 #TODO chmod a+x this file
 file_count="$(git diff --cached --numstat | wc -l)"
-if "$file_count" != "0"
+if [ "$file_count" != "0" ]
 then
   git commit -am "Rescaled images (Github Action)"
   git push
